17c8f4c6f131ed02d21a7f744c72afd0416bfc6c
Nazar Prysiazhnyi <2008Nazar@gmail.com>
2025-03-10 19:28:08 +0200

    [chor] setup project

---
+++ ConcurrencyInSwift/ConcurrencyInSwift/Aplication/ConcurrencyInSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConcurrencyInSwiftApp.swift
+//  ConcurrencyInSwift
+//
+//  Created by Nazar Prysiazhnyi on 10.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct ConcurrencyInSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
